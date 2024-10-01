a5054ae616737d3ca6fdd76d7bb20312490760dc
anwrat <anweshrwt22@gmail.com>
2024-10-01 06:17:31 +0545

    Configuring Tailwind CSS

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Working on an image uploader in React
+
+# For configuring tailwind
+<img width="653" alt="image" src="https://github.com/user-attachments/assets/13b3ded6-2d23-4382-bd99-760f6ec95732">
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-import './App.css'
+import './index.css'
 
 function App() {
 
   return (
     <>
-
+      <h1 className="text-3xl font-bold underline text-red-600">Image Uploader</h1>
     </>
   )
 }
